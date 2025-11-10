01441a785f7587fb1d25c07ad5e3fb1020d7a6d1
CodyMarkix <marekplasek08@protonmail.com>
2025-11-10 09:36:49 +0100

    basic script done

--- weatherd.py
+++ weatherd.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
-import os
+import os, sys
 import json
+import requests
+import time
+import argparse
 
 config_paths = [
-	"~/.config/weatherd/config.json",
+	f"{os.environ['HOME']}/.config/weatherd/config.json",
 	"/etc/weatherd/config.json"
 ]
+
+default_config = {
+	"latitude": 50.088,
+	"longitude": 14.4208,
+	"timezone": "Europe/Prague",
+	"temperature_unit": "celsius",
+	"frequency": 300,
+	"prettify_output": True
+}
+
+
+parser = argparse.ArgumentParser(description='Weather daemon for fetching data from Open-Meteo')
+parser.add_argument('-S', '--systemd', action="store_true")
+args = parser.parse_args()
 
 def checkConfigPath() -> str:
 	for path in config_paths:
@@ -14,20 +31,67 @@ def checkConfigPath() -> str:
 
 def loadConfig(path: str):
 	if path == None:
-		return {
-			"latitude": 50.088,
-			"longitude": 14.4208,
-			"timezone": "Europe/Prague",
-			"temperature_unit": "celsius",
-			"frequency": 300
-		}
+		return default_config
 	with open(path, 'r', encoding='utf-8') as config_raw:
 		config_json = json.load(config_raw)
 		return config_json
 
+def generateSystemdUnit():
+	systemd_service = """[Unit]
+Description=weatherd service
+
+[Service]
+Type=Simple
+ExecStart=~/.local/bin/weatherd
+
+[Install]
+WantedBy=multi-user.target
+"""
+	
+	os.system(f"mkdir -p {os.environ['HOME']}/.config/systemd/user")
+	with open(f"{os.environ['HOME']}/.config/systemd/user/weatherd.service", "w") as f:
+		f.write(systemd_service)
+	
+	print("Systemd unit generated! Run \u001b[1msystemd --user daemon-reload && systemd --user --now enable weatherd.service\u001b[0m to start weatherd.")
+
 def main():
-	config = loadConfig(checkConfigPath())
-	print(config)
+	config = {**default_config, **loadConfig(checkConfigPath())}
+	running = 1
+
+	if not os.path.exists(f"{os.environ['HOME']}/.var"):
+		os.sys(f"mkdir -p {os.environ['HOME']}/.var") 
+
+	while running:
+		try:
+			req = requests.get("https://api.open-meteo.com/v1/forecast", params = {
+				"latitude": config['latitude'],
+				"longitude": config['longitude'],
+				"timezone": config['timezone'],
+				"temperature_unit": config['temperature_unit'],
+				"current": "temperature_2m,weather_code",
+				"hourly": "weather_code,temperature_2m,precipitation_probability"
+			})
+			req_json = req.json()
+		
+			output = {
+				"current": {
+					"time": req_json['current']['time'],
+					"temperature": req_json['current']['temperature_2m'],
+					"weather_code": req_json['current']['weather_code'],
+				},
+				"hourly": req_json['hourly']
+			}
+			
+			with open(f"{os.environ['HOME']}/.var/weatherd.json", "w") as f:
+				json.dump(output, f, indent=4 if config['prettify_output'] else None)
+
+			time.sleep(config['frequency'])
+		except KeyboardInterrupt:
+			running = 0
 
 if __name__ == "__main__":
+	if args.systemd:
+		generateSystemdUnit()
+		sys.exit()
+		
 	main()
